d79218ed7f08c08ed5a09a5aa75bf73f010c51cd
Fatih YOLCU <128999435+kilicsizoglu@users.noreply.github.com>
2023-09-09 16:41:56 +0300

    Add project files.

---
+++ JsonMinimizer.cs
@@ -0,0 +1,67 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace JsonMinimizer
+{
+    public class JsonMinimizer
+    {
+        private bool StringProcess = false;
+        private StringReader? stringReader;
+        private StringBuilder? stringBuilder;
+        public JsonMinimizer(string? jsonData)
+        {
+            if (jsonData != null)
+            {
+                stringReader = new StringReader(jsonData);
+                stringBuilder = new StringBuilder();
+            }
+        }
+
+        public string getString()
+        {
+            if (stringBuilder != null)
+            {
+                return stringBuilder.ToString();
+            }
+            return "";
+        }
+
+        public void Minimize()
+        {
+            if (stringReader != null)
+            {
+                int ch;
+                while (true)
+                {
+                    ch = stringReader.Read();
+                    if (ch == -1)
+                    {
+                        break;
+                    }
+                    if ((char)ch == '"')
+                    {
+                        StringProcess = !StringProcess;
+                    }
+                    if (StringProcess == true)
+                    {
+                        if (stringBuilder != null)
+                        {
+                            stringBuilder.Append((char) ch);
+                        }
+                    } 
+                    if (StringProcess == false)
+                    {
+                        if (ch == ' ')
+                        {
+                            continue;
+                        }
+                        if (stringBuilder != null)
+                        {
+                            stringBuilder.Append((char) ch);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
